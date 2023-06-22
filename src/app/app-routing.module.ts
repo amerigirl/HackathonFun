85d4c2e5765432ef5d888e3d2b9002b84a661e65
Be-Asia <amerigirl29@gmail.com>
2023-06-22 11:53:27 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivityComponent } from './layout/activity/activity.component';
+import { CodeComponent } from './layout/code/code.component';
+import { IssuesComponent } from './layout/issues/issues.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'overview', component: OverviewPageComponent },
+  {path: 'issues', component: IssuesComponent },
+  {path: 'code', component: CodeComponent },
+  {path: 'activity', component: ActivityComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { TopNavComponent } from './layout/top-nav/top-nav.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { IssuesComponent } from './layout/issues/issues.component';
+import { CodeComponent } from './layout/code/code.component';
+import { ActivityComponent } from './layout/activity/activity.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    OverviewPageComponent,
+    IssuesComponent,
+    CodeComponent,
+    ActivityComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
